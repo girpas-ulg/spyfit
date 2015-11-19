d2a40a0bb65997321fec07042317123f58fa7947
Benoit Bovy <bbovy@ulg.ac.be>
2015-11-19 16:30:17 +0100

    added module doctrings

--- sfit4py/io/__init__.py
+++ sfit4py/io/__init__.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+"""
+The `io` sub-package provides functions to read (write) SFIT4
+input and output files into (from) very basic, common data structures
+(i.e., Numpy arrays and Python dictionaries).
+
+TODO: all functions should use a consistent data model here.
+Maybe follow a 'minimal' netCDF data model (coordinates, variables and
+attributes). See how `xray` datasets dictionary arguments must be arranged.
+
+"""
 
 from .inputs import *
 from .outputs import *
