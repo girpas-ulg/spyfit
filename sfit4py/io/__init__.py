55fd4a35d25923ad0273d20b4fec8d9c75c06dbd
Benoit Bovy <bbovy@ulg.ac.be>
2015-12-14 11:42:49 +0100

    merged sfit4 ascii io functions into a single python module

--- sfit4py/io/__init__.py
+++ sfit4py/io/__init__.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 
+"""Low level IO routines.
+
+The `io` sub-package provides functions to read/write various formats
+(e.g., SFIT4 I/O ascii files, GEOMS hdf4) into/from very basic,
+common data structures (i.e., Python dictionaries and Numpy arrays).
+
+The returned dictionaries have the following, xray-friendly structure,
+which follow the Common Data Model (CDM)::
+
+    d = {
+        'variables': {
+            'var1': (('x', 'y'), data, {'units': 'kg',}),
+        },
+        'coords': {
+            'x': (xdata, {'units': 'cm'}),
+            'y': (ydata, {'units': 'cm'}),
+        },
+        'attrs': {}
+    }
+
 """
-The `io` sub-package provides functions to read (write) SFIT4
-input and output files into (from) very basic, common data structures
-(i.e., Numpy arrays and Python dictionaries).
 
-TODO: all functions should use a consistent data model here.
-Maybe follow a 'minimal' netCDF data model (coordinates, variables and
-attributes). See how `xray` datasets dictionary arguments must be arranged.
-proposition: return dict structured as
-d = {
-    'coordinates': [
-        {'name': '', ''points': array, 'bounds': array,
-         'attributes': {'name'...}},
-    ],
-    'variables': [
-        {'name': '', 'data': array, 'dim_coords': ((0, 'name'), (1, 'name')...),
-         'attributes' : {...}},
-    ],
-    'global_attributes': {}
-}
-
-"""
-
-from .inputs import *
-from .outputs import *
+from .sfit4ascii import *
--- sfit4py/io/sfit4ascii.py
+++ sfit4py/io/sfit4ascii.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""
-Functions for reading the SFIT4 output text files into
-Python dictionaries and Numpy arrays.
-
-"""
+"""Read (write) SFIT4 input and output ascii files."""
 
 import datetime
 import re
@@ -17,8 +13,13 @@ import numpy as np
 __all__ = ['read_matrix', 'read_table', 'read_profiles',
            'read_state_vector', 'read_param_iterations',
            'read_spectra', 'read_single_spectrum', 'read_solar_spectrum',
-           'read_summary']
+           'read_summary', 'read_layers', 'read_reference_profiles',
+           'read_spectrum']
 
+
+# ---------------------------------------
+# output files
+# ---------------------------------------
 
 HEADER_PATTERN = (r"\s*SFIT4:V(?P<sfit4_version>[0-9.]+)"
                   r"[\w\s:-]*RUNTIME:(?P<runtime>[0-9:\-]+)"
@@ -398,3 +399,131 @@ def read_summary(filename):
         outputd.update({k: f(v) for k, f, v in zip(ckeys, cfuncs, cvals)})
 
     return outputd
+
+
+# ---------------------------------------
+# input files
+# ---------------------------------------
+
+REF_GAZ_PATTERN = r"\s*(?P<index>\d+)\s+(?P<name>\w+)\s+(?P<description>.+)"
+
+
+def read_layers(filename):
+    """
+    Read profile layers.
+
+    Use this function to load 'in.stalayers'.
+
+    """
+    with open(filename, 'r') as f:
+        outputd = dict()
+        outputd['header'] = f.readline().strip()
+        n_layers = int(f.readline())
+        dummy = f.readline()
+
+        data = np.loadtxt(f)
+        assert data.shape[0] == n_layers
+        index, lbound, thick, growth, points = data.transpose()
+
+        outputd['index'] = index[:-1].astype('i')
+        outputd['altitude'] = {
+            'points': points[:-1],
+            'lower_bounds': lbound
+        }
+
+    return outputd
+
+
+def read_reference_profiles(filename):
+    """
+    Read coordinates, atmosphere and gas reference profiles.
+
+    Use this function to load 'in.refprofile'.
+
+    """
+    with open(filename, 'r') as f:
+        outputd = dict()
+        order_desc, n_levels, n_gases = map(int, f.readline().split())
+        if order_desc:
+            order = 'descending'
+        else:
+            order = 'ascending'
+
+        # altitude, pressure and temperature profiles
+        for profile in ('altitude', 'pressure', 'temperature'):
+            description = f.readline().strip()
+            data = np.fromfile(f, count=n_levels, sep=" ")
+            outputd[profile] = {
+                'points': data,
+                'attrs': {
+                    'description': description,
+                    'order': order
+                }
+            }
+
+        # gas profiles
+        gases = []
+        for iprofile in range(n_gases):
+            header = re.match(REF_GAZ_PATTERN, f.readline()).groupdict()
+            data = np.fromfile(f, count=n_levels, sep=" ")
+            if header['name'] == 'OTHER':
+                data = np.array([])
+            gas = {
+                'name': '{}_reference_profile'.format(header['name']),
+                'data': data,
+                'attrs': {
+                    'gas': header['name'],
+                    'gas_index': int(header['index']),
+                    'description': header['description'].strip(),
+                    'order': order
+                }
+            }
+            gases.append(gas)
+
+        outputd['gases'] = gases
+
+    return outputd
+
+
+def read_spectrum(filename):
+    """
+    Read a spectrum (bands and scans data).
+
+    Use this function to load 'in.spectrum'.
+
+    """
+    with open(filename, 'r') as f:
+        spectra = []
+
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            sza, earth_radius, lat, lon, snr = map(float, line.split())
+
+            dt_items = list(map(int, re.split('[\s\.]+', f.readline())[:-1]))
+            dt_items[-1] *= 10     # convert decimal seconds to milliseconds
+            dt = datetime.datetime(*dt_items)
+
+            title = f.readline().strip()
+
+            wn_min, wn_max, wn_step, size = map(eval, f.readline().split())
+
+            data = np.fromfile(f, count=size, sep=" ")
+            wavenumber = np.arange(wn_min, wn_max + wn_step / 2., wn_step)
+
+            spectra.append({
+                'data': data,
+                'wavenumber': wavenumber,  # TODO: treat it as a coordinate
+                'attrs': {
+                    'title': title,
+                    'solar_zenith_angle': sza,
+                    'earth_radius': earth_radius,
+                    'latitude': lat,
+                    'longitude': lon,
+                    'snr': snr,
+                    'datetime': dt
+                }
+            })
+
+    return {'spectra': spectra}
